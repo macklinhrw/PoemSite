bb70f76dee9a2c423f07e18f27fb0fc183f78a84
macklinhrw <macklingaming@gmail.com>
2021-12-25 12:57:21 -0800

    add back button and image to poem page

--- src/pages/poems/[slug].tsx
+++ src/pages/poems/[slug].tsx
@@ -1,47 +1,62 @@
 import { useRouter } from "next/dist/client/router";
 import {
-    GetAllPoemsDocument,
+  GetAllPoemsDocument,
   GetPoemBySlugDocument,
   useGetPoemBySlugQuery,
 } from "../../generated/graphql";
 import { Navbar } from "../../components/Nav/Navbar";
-import { VStack, Box, Text, Flex } from "@chakra-ui/react";
+import { VStack, Box, Text, Flex, Img, Button, Container } from "@chakra-ui/react";
 import { client } from "../_app";
 
 const PoemsPage = (props) => {
   const router = useRouter();
   const { slug } = router.query;
-  const { poem } = props
+  const { poem } = props;
 
   return (
     <>
       <Navbar />
+      <Container maxW="container.sm">
+        <Button mt={4} onClick={() => router.back()}>
+          Back
+        </Button>
+      </Container>
       <Box mx="auto" w="max" maxW="100%">
         {poem && (
-          <VStack mt="10">
-            <Text
-              as="h1"
-              fontSize={{ base: "xl", lg: "2xl" }}
-              fontWeight="bold"
-              fontFamily="serif"
-            >
-              {poem.title}
-            </Text>
-            <Text wordBreak="break-word">
-              {poem.content.split("\n").map((line) => {
-                return (
-                  <Text
-                    wordBreak="break-word"
-                    textAlign="center"
-                    fontSize={{ sm: "md", md: "lg", lg: "xl" }}
-                    fontFamily="serif"
-                  >
-                    {line}
-                  </Text>
-                );
-              })}
-            </Text>
-          </VStack>
+          <Flex align="center" flexDir="column">
+            <VStack mt="8">
+              <Text
+                as="h1"
+                fontSize={{ base: "xl", lg: "2xl" }}
+                fontWeight="bold"
+                fontFamily="serif"
+              >
+                {poem.title}
+              </Text>
+              <Text wordBreak="break-word">
+                {poem.content.split("\n").map((line) => {
+                  return (
+                    <Text
+                      wordBreak="break-word"
+                      textAlign="center"
+                      fontSize={{ sm: "md", md: "lg", lg: "xl" }}
+                      fontFamily="serif"
+                    >
+                      {line}
+                    </Text>
+                  );
+                })}
+              </Text>
+            </VStack>
+            <Img
+              src={poem.imageLink}
+              alt={poem.title}
+              h="60"
+              w="80"
+              rounded="md"
+              mt={6}
+            />
+          </Flex>
         )}
       </Box>
     </>
